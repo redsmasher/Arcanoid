c7d0d1711db6a1a02770e2aed0875f2cb6dfbece
DESKTOP-9EJ05EC\ichig <ichigo77@hotmail.gr>
2017-09-03 23:58:54 +0300

    Arcanoid 2: Making the menu system

---
+++ Arcanoid/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+	public void LoadLevel(string name)
+	{
+		SceneManager.LoadScene(name);
+	}
+}
