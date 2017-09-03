24639d1cd989c56d028b4b4f221c2969890738af
DESKTOP-9EJ05EC\ichig <ichigo77@hotmail.gr>
2017-09-04 00:04:17 +0300

    Arcanoid 2: Playing background music

---
+++ Arcanoid/Assets/Scripts/MusicManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour 
+{
+	static MusicManager musicManager;
+	private AudioSource audioSource = null;
+
+	void Awake () 
+	{
+		audioSource = GetComponent<AudioSource> ();
+
+		if (musicManager != null) 
+		{
+			Destroy (gameObject);
+		} 
+		else 
+		{
+			musicManager = this;
+			GameObject.DontDestroyOnLoad (musicManager);
+		}
+	}
+
+	void Update () 
+	{
+		
+	}
+}
