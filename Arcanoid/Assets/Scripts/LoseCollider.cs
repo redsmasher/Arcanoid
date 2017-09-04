7d07a16d6508b044d334f4de0bde7565c59f58bf
DESKTOP-9EJ05EC\ichig <ichigo77@hotmail.gr>
2017-09-04 13:21:25 +0300

    Arcanoid 6:Movement by mouse

--- Arcanoid/Assets/Scripts/LoseCollider.cs
+++ Arcanoid/Assets/Scripts/LoseCollider.cs
@@ -2,15 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LoseCollider : MonoBehaviour {
+public class LoseCollider : MonoBehaviour
+{
+	public LevelManager levelManager;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+	void OnTriggerEnter2D()
+	{
+		levelManager.LoadLevel ("WinScreen");
 	}
 }
---
+++ Arcanoid/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour 
+{
+	private float mousePosInBlocks;
+
+	void Start () 
+	{
+		
+	}
+
+	void Update () 
+	{
+		Vector3 paddlePos = new Vector3 (this.transform.position.x, this.transform.position.y, 0f);
+		mousePosInBlocks = Input.mousePosition.x / Screen.width * 16;
+		paddlePos.x = Mathf.Clamp(mousePosInBlocks,0f,9.80f);
+		this.transform.position = paddlePos;
+	}
+}
