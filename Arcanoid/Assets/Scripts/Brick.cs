c8ffdb2e8f11b01bd259e1ef7a5de01f57f93bf7
DESKTOP-9EJ05EC\ichig <ichigo77@hotmail.gr>
2017-09-04 13:47:42 +0300

    Arcanoid 8:Implemanting a simple health system

---
+++ Arcanoid/Assets/Scripts/Brick.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Brick : MonoBehaviour 
+{
+	public int maxHits;
+
+	private int currentHit;
+
+	void OnCollisionEnter2D(Collision2D col)
+	{
+		currentHit++;
+		if (currentHit >= maxHits) 
+		{
+			Destroy (gameObject);
+		}
+	}
+
+	void Update ()
+	{
+		
+	}
+}
