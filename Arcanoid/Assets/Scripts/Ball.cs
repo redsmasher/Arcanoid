bf079b0b81af87ca460cf1a69cecda02fbee370a
DESKTOP-9EJ05EC\ichig <ichigo77@hotmail.gr>
2017-09-04 13:34:02 +0300

    Arcanoid 7:Launching ball on Mouse Click

---
+++ Arcanoid/Assets/Scripts/Ball.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+	private Rigidbody2D rb;
+	private PlayerController playerController;
+	private Vector3 ballToPaddle;
+	private bool hasStarted = false;
+
+	void Start () 
+	{
+		rb = GetComponent<Rigidbody2D> ();
+		playerController = FindObjectOfType<PlayerController> ();
+		ballToPaddle = this.transform.position - playerController.transform.position; 
+	}
+
+	void Update () 
+	{
+		if (!hasStarted)
+		{
+			this.transform.position = playerController.transform.position + ballToPaddle;
+
+			if (Input.GetKeyDown (KeyCode.Mouse0)) 
+			{
+				hasStarted = true;
+				this.rb.velocity = new Vector2 (2f,10f);
+			}
+		}
+	}
+}
